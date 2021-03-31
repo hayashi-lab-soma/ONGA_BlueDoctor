2667914dfc6595ea1461b2804a5ba310475ea21f
Ayumu <tommy.tommy.walker@gmail.com>
2021-03-31 22:31:06 +0900

    Mod Only Mainwindow ver.

--- Sample_basic_gui/Sample_basic_gui_app/src/mainwindow.cpp
+++ Sample_basic_gui/Sample_basic_gui_app/src/mainwindow.cpp
@@ -13,3 +13,8 @@ MainWindow::~MainWindow()
 	delete ui;
 }
 
+void MainWindow::closeEvent(QCloseEvent *event)
+{
+	qInfo() << "Bye bye";
+}
+
--- Sample_basic_gui/Sample_basic_gui_app/src/mainwindow.h
+++ Sample_basic_gui/Sample_basic_gui_app/src/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDebug>
+#include <QString>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +19,8 @@ public:
 
 private:
 	Ui::MainWindow *ui;
+
+protected:
+	void closeEvent(QCloseEvent *event);
 };
 #endif // MAINWINDOW_H
